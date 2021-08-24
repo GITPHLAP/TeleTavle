b7ffd6e9df4f2b82bec8248df65c2847934a1a3f
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2021-08-24 14:54:20 +0200

    Added featuredSnippet

--- TeleTavleLibrary/TeleTavleLibrary/SearchResult.cs
+++ TeleTavleLibrary/TeleTavleLibrary/SearchResult.cs
@@ -10,10 +10,12 @@ namespace TeleTavleLibrary
         int rank;
         string searchWord;
         string url;
+        string featuredSnippet;
 
         public int Rank { get => rank; set => rank = value; }
         public string SearchWord { get => searchWord; set => searchWord = value; }
         public string Url { get => url; set => url = value; }
+        public string FeaturedSnippet { get => featuredSnippet; set => featuredSnippet = value; }
         public string UrlLocation
         {
             get
