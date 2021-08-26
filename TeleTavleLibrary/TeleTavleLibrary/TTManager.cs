a5574be013e44b20b791424f189de4b85a4d4eb0
PhilipSKP <kjedtoft@gmail.com>
2021-08-26 15:57:37 +0200

    Performance improvements in TTManager

--- TeleTavleLibrary/TeleTavleLibrary/TTManager.cs
+++ TeleTavleLibrary/TeleTavleLibrary/TTManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TeleTavleLibrary
 {
@@ -42,12 +43,67 @@ namespace TeleTavleLibrary
                         IndexURL(result.Url);
                     }
                 }
-                
+
             }
             else
             {
                 NewLogEvent(this, new LogEventArgs("Der er ingen søgeord i listen, kan ikke fortsætte...", InformationType.Warning));
             }
+        }
+
+        public async Task StartProcessParallelAsync(List<string> searchWords)
+        {
+            if (searchWords.Count > 0 || searchWords != null)
+            {
+
+                List<Task<List<SearchResult>>> tasks = new List<Task<List<SearchResult>>>();
+
+                Parallel.ForEach(searchWords, searchWord => { tasks.Add(Task.Run(() => GetSearchInformation(searchWord))); });
+
+                List<SearchResult>[] searchResults = await Task.WhenAll(tasks);
+
+                Parallel.ForEach(searchResults, searchResult =>
+                    {
+                        IndexPingAndFindSEF(searchResult);
+                    });
+
+            }
+            else
+            {
+                NewLogEvent(this, new LogEventArgs("Der er ingen søgeord i listen, kan ikke fortsætte...", InformationType.Warning));
+            }
+        }
+
+        //TODO: Refactory this method name dont make sense 
+        void IndexPingAndFindSEF(List<SearchResult> searchResult)
+        {
+            foreach (SearchResult result in searchResult)
+            {
+                SearchResultSEF sefSearchResult = SEFInformation(result);
+
+                //TODO: TEST skal fjernes
+                sefSearchResult.Header = "TEST123";
+
+                //Ping the result
+                PingSearchResult(sefSearchResult);
+
+                IndexURL(result.Url);
+            }
+        }
+
+        //TODO:Test SKAL FJERNES
+        List<SearchResult> GetSearchInformationTest(string searchWords)
+        {
+            List<SearchResult> test = new List<SearchResult>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                test.Add(new SearchResult { Url = "https://rainbow.simbascorner.dk/", Rank = i, SearchWord = searchWords });
+            }
+            Task.Delay(800);
+
+
+            return test;
         }
 
         List<SearchResult> GetSearchInformation(string searchWords)
@@ -99,7 +155,7 @@ namespace TeleTavleLibrary
         {
             LogEvent?.Invoke(sender, e);
 
-            //Console.WriteLine($"{e.Time}: {e.Message}");
+            Console.WriteLine($"{e.Time}: {e.Message}");
 
         }
     }
