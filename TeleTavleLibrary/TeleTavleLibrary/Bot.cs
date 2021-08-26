810a2818f64141eaa4fad13dc479713b8a84f1e2
PhilipSKP <kjedtoft@gmail.com>
2021-08-26 15:53:32 +0200

    Hide diagnostic and disable console from ChromeDriver

--- TeleTavleLibrary/TeleTavleLibrary/Bot.cs
+++ TeleTavleLibrary/TeleTavleLibrary/Bot.cs
@@ -14,15 +14,25 @@ namespace TeleTavleLibrary
         {
             try
             {
+                //Chrome service to supress diagnostic and hide console
+                ChromeDriverService cDS = ChromeDriverService.CreateDefaultService();
+                cDS.SuppressInitialDiagnosticInformation = true;
+                cDS.HideCommandPromptWindow = true;
+                //Chrome options 
                 ChromeOptions co = new ChromeOptions();
-                co.AddArguments("--headless");
-                IWebDriver chromeDriver = new ChromeDriver(co);
+                co.AddArguments("headless");
+                //This prevent reCAPTCHAR
+                co.AddArguments("--user-agent=Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 92.0.4515.131 Safari / 537.36");
+                //do not log so much with this code
+                co.AddArgument("--log-level=3");
 
-                return chromeDriver; 
+                IWebDriver chromeDriver = new ChromeDriver(cDS, co);
+
+                return chromeDriver;
             }
             catch (Exception e)
             {
-                NewLogEvent(new LogEventArgs($"Kunne ikke oprette chromedriver... {e}",InformationType.Failed));
+                NewLogEvent(new LogEventArgs($"Kunne ikke oprette chromedriver... {e}", InformationType.Failed));
                 return null;
             }
 
