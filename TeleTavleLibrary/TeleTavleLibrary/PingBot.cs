c07ae67402274f9740b67a9c0324580cb5a5ae68
PhilipSKP <kjedtoft@gmail.com>
2021-09-14 12:59:15 +0200

    Fix: always set sef header to sef url in pingbot

--- TeleTavleLibrary/TeleTavleLibrary/PingBot.cs
+++ TeleTavleLibrary/TeleTavleLibrary/PingBot.cs
@@ -18,7 +18,7 @@ namespace TeleTavleLibrary
 
             //if (!string.IsNullOrEmpty(searchResultSEF.Header))
             //{
-                CreateAndSavePing(searchResultSEF, chromeDriver);
+            CreateAndSavePing(searchResultSEF, chromeDriver);
             //}
 
             try
@@ -42,12 +42,13 @@ namespace TeleTavleLibrary
         {
             try
             {
-                if (!string.IsNullOrEmpty(searchResultSEF.Header))
+                if (string.IsNullOrEmpty(searchResultSEF.Header))
                 {
                     searchResultSEF.Header = searchResultSEF.SearchResult.UrlLocation;
                 }
-                    //Click new ping
-                    chromeDriver.FindElement(By.CssSelector("#toolbar-new > button")).Click();
+
+                //Click new ping
+                chromeDriver.FindElement(By.CssSelector("#toolbar-new > button")).Click();
                 //Find the inputs and send title and url
                 chromeDriver.FindElement(By.Id("title")).SendKeys(searchResultSEF.Header);
                 chromeDriver.FindElement(By.Id("linkurl")).SendKeys(searchResultSEF.SearchResult.Url);
