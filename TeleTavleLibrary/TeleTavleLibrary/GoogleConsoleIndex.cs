b94242bac6f5359a8920fa76d0bc3565ebe56223
PhilipSKP <kjedtoft@gmail.com>
2021-09-27 09:39:49 +0200

    Clean up and remove unused code in GoogleIndex

--- TeleTavleLibrary/TeleTavleLibrary/GoogleConsoleIndex.cs
+++ TeleTavleLibrary/TeleTavleLibrary/GoogleConsoleIndex.cs
@@ -45,49 +45,12 @@ namespace TeleTavleLibrary
         }
 
         /// <summary>
-        /// Sends a request to google about indexing. action could be URL_UPDATED OR URL_DELETED. Make sure to await, so the request is done.  
+        /// Sends a batch request to google about indexing. action could be URL_UPDATED OR URL_DELETED. Make sure to await, so the request is done.  
         /// Use .result after method
         /// </summary>
-        /// <param name="URLToIndex"></param>
-        /// <param name="action"></param>
+        /// <param name="URLsToIndex"></param>
+        /// <param name="action"> URL_UPDATED or URL_DELETED</param>
         /// <returns></returns>
-        public PublishUrlNotificationResponse IndexURL(string URLToIndex, string action)
-        {
-            if (googleCredential == null)
-            {
-                return null;
-            }
-            var credential = googleCredential.UnderlyingCredential;
-            //Adding credentials
-            var googleIndexingApiClientService = new IndexingService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential
-            });
-            //The body of the post request
-            var requestBody = new UrlNotification
-            {
-                Url = URLToIndex,
-                Type = action
-            };
-
-            var publishRequest = new UrlNotificationsResource.PublishRequest(googleIndexingApiClientService, requestBody);
-
-            try
-            {
-                //If something goes wrong, it will throw exception
-                var executedRequest = publishRequest.ExecuteAsync().Result;
-                LogEvent?.Invoke(this, new LogEventArgs($"{URLToIndex} , er indexeret", InformationType.Successful));
-                return executedRequest;
-            }
-            catch (Exception e)
-            {
-                LogEvent?.Invoke(this, new LogEventArgs($"GoogleConsoleIndex kunne ikke sende anmodning om indeksering...URL:{URLToIndex}   {e}",
-                    InformationType.Failed));
-            }
-            return null;
-        }
-
-
         public async Task<List<PublishUrlNotificationResponse>> IndexBatchURL(List<string> URLsToIndex, string action)
         {
             if (googleCredential == null)
@@ -95,34 +58,31 @@ namespace TeleTavleLibrary
                 return null;
             }
             var credential = googleCredential.UnderlyingCredential;
+
             //Adding credentials
-            var googleIndexingApiClientService = new IndexingService(new BaseClientService.Initializer
+            IndexingService googleIndexingApiClientService = new IndexingService(new BaseClientService.Initializer
             {
                 HttpClientInitializer = credential
             });
 
-            var request = new BatchRequest(googleIndexingApiClientService);
+            BatchRequest request = new BatchRequest(googleIndexingApiClientService);
 
             List<PublishUrlNotificationResponse> notificationResponses = new List<PublishUrlNotificationResponse>();
 
-            foreach (var URLToIndex in URLsToIndex)
+            foreach (string URLToIndex in URLsToIndex)
             {
-
-
                 //The body of the post request
-                var requestBody = new UrlNotification
+                UrlNotification requestBody = new UrlNotification
                 {
                     Url = URLToIndex,
                     Type = action
                 };
 
                 request.Queue<PublishUrlNotificationResponse>(
-               new UrlNotificationsResource.PublishRequest(googleIndexingApiClientService, requestBody), (response, error, i, message) =>
+               new PublishRequest(googleIndexingApiClientService, requestBody), (response, error, i, message) =>
                {
                    notificationResponses.Add(response);
                });
-
-
             }
             try
             {
